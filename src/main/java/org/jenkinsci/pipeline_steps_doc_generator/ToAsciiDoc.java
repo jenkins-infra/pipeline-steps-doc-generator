0fe6d56a3e90e8f6f7a18996dbb139a1c8e10f02
Zbynek Konecny <zbynek1729@gmail.com>
2019-11-26 23:34:17 +0100

    Consistent naming of "Plugin site"
    Co-Authored-By: Oleg Nenashev <o.v.nenashev@gmail.com>

--- src/main/java/org/jenkinsci/pipeline_steps_doc_generator/ToAsciiDoc.java
+++ src/main/java/org/jenkinsci/pipeline_steps_doc_generator/ToAsciiDoc.java
@@ -252,7 +252,7 @@ public class ToAsciiDoc {
 
         whole9yards.append("== ").append(displayName).append("\n\n");
         if (!"core".equals(pluginName)) {
-            whole9yards.append("plugin:").append(pluginName).append("[View this plugin on the Plugins Index]\n\n");
+            whole9yards.append("plugin:").append(pluginName).append("[View this plugin on the Plugins site]\n\n");
         }
         for(String type : byPlugin.keySet()){
             for (QuasiDescriptor sd : byPlugin.get(type)){
