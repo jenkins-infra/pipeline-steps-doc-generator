ac152c7f8ac4977e077f42b89a078762637b416a
Mark Waite <mark.earl.waite@gmail.com>
2021-12-13 06:50:54 -0700

    Use loadClass instead of removed _findLoadedClass
    Deprecation message suggested the replacement.

--- src/main/java/org/jenkinsci/pipeline_steps_doc_generator/HyperLocalPluginManger.java
+++ src/main/java/org/jenkinsci/pipeline_steps_doc_generator/HyperLocalPluginManger.java
@@ -236,7 +236,7 @@ public class HyperLocalPluginManger extends LocalPluginManager{
             if (FAST_LOOKUP) {
                 for (PluginWrapper p : activePlugins) {
                     try {
-                        Class<?> c = ClassLoaderReflectionToolkit._findLoadedClass(p.classLoader, name);
+                        Class<?> c = ClassLoaderReflectionToolkit.loadClass(p.classLoader, name);
                         if (c != null) {
                             synchronized (loaded) {
                                 loaded.put(name, c);
@@ -247,7 +247,7 @@ public class HyperLocalPluginManger extends LocalPluginManager{
                             return c;
                         }
                         // calling findClass twice appears to cause LinkageError: duplicate class def
-                        c = ClassLoaderReflectionToolkit._findClass(p.classLoader, name);
+                        c = ClassLoaderReflectionToolkit.loadClass(p.classLoader, name);
                         synchronized (loaded) {
                             loaded.put(name, c);
                         }
