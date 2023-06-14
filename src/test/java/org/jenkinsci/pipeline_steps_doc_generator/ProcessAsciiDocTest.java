b634ff8834ae4fd21dcae3a670253c3f6ce65161
Mark Waite <mark.earl.waite@gmail.com>
2023-06-13 22:38:51 -0600

    More simplification

--- src/test/java/org/jenkinsci/pipeline_steps_doc_generator/ProcessAsciiDocTest.java
+++ src/test/java/org/jenkinsci/pipeline_steps_doc_generator/ProcessAsciiDocTest.java
@@ -1,26 +1,37 @@
 package org.jenkinsci.pipeline_steps_doc_generator;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.io.FileMatchers.aReadableFile;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
 
 import java.io.File;
-import java.io.IOException;
-import org.apache.commons.io.FileUtils;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.After;
 import org.junit.Test;
 
 public class ProcessAsciiDocTest {
     private String allAscii = "src/test/resources/input/";
-    private File allAsciiDir = new File(allAscii);
-    private File child = new File(allAsciiDir, "workflow-scm.adoc");
+    private File child = new File(allAscii + "workflow-scm.adoc");
 
     private String GIT_SCM_PARAMS_NAME = allAscii + "params/gitscm.adoc";
+    private String GIT_SCM_COMPARE_NAME = GIT_SCM_PARAMS_NAME.replace("params", "compare");
+
+    private ProcessAsciiDoc pad = new ProcessAsciiDoc();
+
+    @After
+    public void deleteGeneratedFiles() {
+        File file = new File(GIT_SCM_PARAMS_NAME);
+        file.delete();
+    }
 
     @Test
     public void isExceptionThrown() {
-        ProcessAsciiDoc pad = new ProcessAsciiDoc();
         assertThrows(RuntimeException.class, () -> {
             pad.separateClass("$class: 'PvcsScm'", allAscii, child, 500);
         });
@@ -28,43 +39,28 @@ public class ProcessAsciiDocTest {
 
     @Test
     public void isNewAdocCreated() throws Exception {
-        ProcessAsciiDoc pad = new ProcessAsciiDoc();
         pad.separateClass("$class: 'GitSCM'", allAscii, child, 500);
-        File file = new File(GIT_SCM_PARAMS_NAME);
-        assertThat(file, aReadableFile());
-        file.delete();
+        assertThat(new File(GIT_SCM_PARAMS_NAME), aReadableFile());
+    }
+
+    private String readFile(String fileName) throws Exception {
+        return new String(Files.readAllBytes(Paths.get(fileName)), StandardCharsets.UTF_8);
     }
 
     @Test
     public void isSeparatedContentCorrect() throws Exception {
-        ProcessAsciiDoc pad = new ProcessAsciiDoc();
         pad.separateClass("$class: 'GitSCM'", allAscii, child, 500);
-        File file1 = new File(GIT_SCM_PARAMS_NAME);
-        File file2 = new File("src/test/resources/input/compare/gitscm.adoc");
-        assertTrue(FileUtils.contentEquals(file1, file2));
-        file1.delete();
+        String generatedContent = readFile(GIT_SCM_PARAMS_NAME);
+        String referenceContent = readFile(GIT_SCM_COMPARE_NAME);
+        assertThat(generatedContent, is(referenceContent));
     }
 
     @Test
-    public void isHyperlinkReplacedCorrectly() {
-        ProcessAsciiDoc pad = new ProcessAsciiDoc();
-        try {
-            String[] lines =
-                    pad.separateClass("$class: 'GitSCM'", allAscii, child, 500).split("\n");
-            String link = "";
-            File file = new File(GIT_SCM_PARAMS_NAME);
-            file.delete();
-            for (String line : lines) {
-                if (line.contains("class: 'GitSCM'")) {
-                    link = line;
-                    break;
-                }
-            }
-            assertEquals(
-                    "<li><span><a href=\"/doc/pipeline/steps/params/gitscm\"><code>$class: 'GitSCM'</code></a></span></li>",
-                    link);
-        } catch (IOException ex) {
-            assertTrue("IOException occurred", false);
-        }
+    public void isHyperlinkReplacedCorrectly() throws Exception {
+        List<String> content = Arrays.asList(
+                pad.separateClass("$class: 'GitSCM'", allAscii, child, 500).split("\n"));
+        String expected =
+                "<li><span><a href=\"/doc/pipeline/steps/params/gitscm\"><code>$class: 'GitSCM'</code></a></span></li>";
+        assertThat(content, hasItem(expected));
     }
 }
