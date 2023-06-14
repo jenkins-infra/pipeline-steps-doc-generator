cc68981b317fe022f2d3842d80a2beeda0be22c9
Janagan Saravanan <122015383+JanaganSaravanan@users.noreply.github.com>
2023-06-14 20:33:19 +0530

    Simplify ProcessAsciiDocTest (#318)
    * Update ProcessAsciiDocTest.java
    
    Replace try/catch sequences in the tests with assertThrows
    
    Replace assertTrue(true) and assertTrue(false) with assertions that state the cause of the failure
    
    * Fix compilation, remove test method renames
    
    * Use hamcrest assertions in tests
    
    * Let isNewAdocCreated throw an exception on error
    
    * Extract common string
    
    * More simplification
    
    ---------
    
    Co-authored-by: Mark Waite <mark.earl.waite@gmail.com>

--- src/test/java/org/jenkinsci/pipeline_steps_doc_generator/ProcessAsciiDocTest.java
+++ src/test/java/org/jenkinsci/pipeline_steps_doc_generator/ProcessAsciiDocTest.java
@@ -1,78 +1,66 @@
 package org.jenkinsci.pipeline_steps_doc_generator;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.io.FileMatchers.aReadableFile;
+import static org.junit.Assert.assertThrows;
 
 import java.io.File;
-import java.io.IOException;
-import org.apache.commons.io.FileUtils;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.After;
 import org.junit.Test;
 
 public class ProcessAsciiDocTest {
-    String allAscii = "src/test/resources/input/";
-    File child = new File(allAscii + "/workflow-scm.adoc");
+    private String allAscii = "src/test/resources/input/";
+    private File child = new File(allAscii + "workflow-scm.adoc");
+
+    private String GIT_SCM_PARAMS_NAME = allAscii + "params/gitscm.adoc";
+    private String GIT_SCM_COMPARE_NAME = GIT_SCM_PARAMS_NAME.replace("params", "compare");
+
+    private ProcessAsciiDoc pad = new ProcessAsciiDoc();
+
+    @After
+    public void deleteGeneratedFiles() {
+        File file = new File(GIT_SCM_PARAMS_NAME);
+        file.delete();
+    }
 
     @Test
     public void isExceptionThrown() {
-        ProcessAsciiDoc pad = new ProcessAsciiDoc();
-        try {
+        assertThrows(RuntimeException.class, () -> {
             pad.separateClass("$class: 'PvcsScm'", allAscii, child, 500);
-            assertTrue(false);
-        } catch (RuntimeException ex) {
-            assertTrue(true);
-        } catch (IOException ex) {
-            assertTrue(false);
-        }
+        });
     }
 
     @Test
-    public void isNewAdocCreated() {
-        ProcessAsciiDoc pad = new ProcessAsciiDoc();
-        try {
-            pad.separateClass("$class: 'GitSCM'", allAscii, child, 500);
-            File file = new File("src/test/resources/input/params/gitscm.adoc");
-            assertTrue(file.exists());
-            file.delete();
-        } catch (RuntimeException | IOException ex) {
-            ex.printStackTrace();
-            assertTrue(false);
-        }
+    public void isNewAdocCreated() throws Exception {
+        pad.separateClass("$class: 'GitSCM'", allAscii, child, 500);
+        assertThat(new File(GIT_SCM_PARAMS_NAME), aReadableFile());
+    }
+
+    private String readFile(String fileName) throws Exception {
+        return new String(Files.readAllBytes(Paths.get(fileName)), StandardCharsets.UTF_8);
     }
 
     @Test
-    public void isSeparatedContentCorrect() {
-        ProcessAsciiDoc pad = new ProcessAsciiDoc();
-        try {
-            pad.separateClass("$class: 'GitSCM'", allAscii, child, 500);
-            File file1 = new File("src/test/resources/input/params/gitscm.adoc");
-            File file2 = new File("src/test/resources/input/compare/gitscm.adoc");
-            assertTrue(FileUtils.contentEquals(file1, file2));
-            file1.delete();
-        } catch (RuntimeException | IOException ex) {
-            assertTrue(false);
-        }
+    public void isSeparatedContentCorrect() throws Exception {
+        pad.separateClass("$class: 'GitSCM'", allAscii, child, 500);
+        String generatedContent = readFile(GIT_SCM_PARAMS_NAME);
+        String referenceContent = readFile(GIT_SCM_COMPARE_NAME);
+        assertThat(generatedContent, is(referenceContent));
     }
 
     @Test
-    public void isHyperlinkReplacedCorrectly() {
-        ProcessAsciiDoc pad = new ProcessAsciiDoc();
-        try {
-            String[] lines =
-                    pad.separateClass("$class: 'GitSCM'", allAscii, child, 500).split("\n");
-            String link = "";
-            File file = new File("src/test/resources/input/params/gitscm.adoc");
-            file.delete();
-            for (String line : lines) {
-                if (line.contains("class: 'GitSCM'")) {
-                    link = line;
-                    break;
-                }
-            }
-            assertEquals(
-                    "<li><span><a href=\"/doc/pipeline/steps/params/gitscm\"><code>$class: 'GitSCM'</code></a></span></li>",
-                    link);
-        } catch (RuntimeException | IOException ex) {
-            assertTrue(false);
-        }
+    public void isHyperlinkReplacedCorrectly() throws Exception {
+        List<String> content = Arrays.asList(
+                pad.separateClass("$class: 'GitSCM'", allAscii, child, 500).split("\n"));
+        String expected =
+                "<li><span><a href=\"/doc/pipeline/steps/params/gitscm\"><code>$class: 'GitSCM'</code></a></span></li>";
+        assertThat(content, hasItem(expected));
     }
 }
